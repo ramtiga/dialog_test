8888a0dabe9f00f048d689ff0f6da327201a653a
dhane31 <dhanegm731@gmail.com>
2021-06-02 22:55:52 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/dialog_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dialog_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Dialog Test'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 190,
+              child: ElevatedButton(
+                child: const Text('AlertDialog'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.indigo,
+                  onPrimary: Colors.white,
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: Text("This is the title"),
+                        content: Text("This is the content"),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text("Cancel"),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                          FlatButton(
+                            child: Text("OK"),
+                            onPressed: () => print('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: 190,
+              child: ElevatedButton(
+                child: const Text('SimpleDialog'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.indigo,
+                  onPrimary: Colors.white,
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return SimpleDialog(
+                        title: Text("This is the title"),
+                        children: [
+                          SimpleDialogOption(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text("first item"),
+                          ),
+                          SimpleDialogOption(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text("second item"),
+                          ),
+                          SimpleDialogOption(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text("Third item"),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: 190,
+              child: ElevatedButton(
+                child: const Text('CupertinoAlertDialog'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.indigo,
+                  onPrimary: Colors.white,
+                ),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (_) => CupertinoAlertDialog(
+                            title: Text("This is the title"),
+                            content: Text("This is the content"),
+                            actions: [
+                              CupertinoButton(
+                                  child: Text('Cancel'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  }),
+                              CupertinoButton(
+                                child: Text('OK'),
+                                onPressed: () {
+                                  print('OK');
+                                },
+                              )
+                            ],
+                          ));
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
